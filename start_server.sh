4e83a137d5b42c195a5948627d2a5ce1ac980230
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-09-06 00:26:32 +0800

    make start the server

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+# /home/ljj/.cache  models downloaded here.
+# 首先应用兼容性补丁
+
+# 然后运行原始启动脚本的内容
+# 你需要把原来 start_server.sh 的内容复制到这里，
+# 但在 Python 命令前加上我们的补丁
 
 iopaint start --model=lama --device=cuda --port=8080
